de718cc90ed1498a17f0a915059d1a9e7d6afb9e
sunkuang <276948241@qq.com>
2026-06-28 14:56:50 +0800

    round 4

--- src/composables/useFavorites.js
+++ src/composables/useFavorites.js
@@ -1,24 +1,27 @@
-import { ref, onMounted } from 'vue'
-import { getFavorites, toggleFavorite as storageToggleFavorite } from '../utils/storage.js'
+import { ref } from 'vue'
+import { getFavorites, saveFavorites } from '../utils/storage.js'
 
 export function useFavorites() {
-  const favoriteIds = ref([])
-
-  function refresh() {
-    favoriteIds.value = getFavorites()
-  }
+  const favoriteIds = ref(getFavorites())
 
   function toggle(drinkId) {
-    favoriteIds.value = storageToggleFavorite(drinkId)
+    const prev = favoriteIds.value
+    const filtered = prev.filter(id => id !== drinkId)
+    if (filtered.length === prev.length) {
+      favoriteIds.value = [...filtered, drinkId]
+    } else {
+      favoriteIds.value = filtered.length ? [...filtered] : []
+    }
+    saveFavorites(favoriteIds.value)
   }
 
   function isFav(drinkId) {
     return favoriteIds.value.includes(drinkId)
   }
 
-  onMounted(() => {
-    refresh()
-  })
+  function refresh() {
+    favoriteIds.value = getFavorites()
+  }
 
   return {
     favoriteIds,
